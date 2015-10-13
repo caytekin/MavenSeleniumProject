731ef73e94d9b92ff55a82dfcafb84d9d2363e85
Cigdem Aytekin <cigdem@info.nl>
2015-10-13 14:34:59 +0200

    surefire reports added

--- src/test/java/com/info/test/FirstTestClassTest.java
+++ src/test/java/com/info/test/FirstTestClassTest.java
@@ -1,9 +1,17 @@
 package com.info.test;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by cigdem.aytekin on 12-10-2015.
@@ -11,13 +19,56 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class FirstTestClassTest {
     WebDriver driver = new FirefoxDriver();
 
+    @Rule
+    public ScreenshotTestRule screenshotTestRule = new ScreenshotTestRule();
+
+
     @Test
-    public void test() throws InterruptedException {
+    public void testBlogSpot() throws InterruptedException {
         driver.manage().window().maximize();
         driver.get("http://only-testing-blog.blogspot.in/2013/11/new-test.html");
         driver.findElement(By.xpath("//input[@name='fname']")).sendKeys("junittest1 executed");
         Thread.sleep(2000);
-        System.out.print("junittest1 class is executed");
+        System.out.println("testBlogSpot class is executed");
         driver.quit();
+    }
+
+    @Test
+    public void testGoogle() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys(new CharSequence[]{"Cheese!"});
+        element.submit();
+        String aTitle = driver.getTitle();
+        System.out.println("Page title is: " + aTitle);
+        assertEquals("Cheese! - Google zoeken", aTitle );
+        driver.quit();
+    }
+    class ScreenshotTestRule implements MethodRule {
+        public Statement apply(final Statement statement, final FrameworkMethod frameworkMethod, final Object o) {
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    try {
+                        statement.evaluate();
+                    } catch (Throwable t) {
+                        captureScreenshot(frameworkMethod.getName());
+                        throw t; // rethrow to allow the failure to be reported to JUnit
+                    }
+                }
+
+                public void captureScreenshot(String fileName) {
+                    try {
+                        new File("target/surefire-reports/").mkdirs(); // Insure directory is there
+                        FileOutputStream out = new FileOutputStream("target/surefire-reports/screenshot-" + fileName + ".png");
+                        out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+                        out.close();
+                    } catch (Exception e) {
+                        // No need to crash the tests if the screenshot fails
+                    }
+                }
+            };
+        }
     }
 }
